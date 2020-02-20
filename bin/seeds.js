21580809be0ae507c7f902fdeccf056490dc7ea0
fernandocomet <fernandocomet@gmail.com>
2020-02-20 16:03:21 +0100

    Changed styles to Bootstrap and views

--- bin/seeds.js
+++ bin/seeds.js
@@ -102,42 +102,145 @@ let races = [{
     imgUrl: "https://2.bp.blogspot.com/-gvubvC8xWV0/WHNKkyir8tI/AAAAAAAAT_o/n1HxPOVvIu8iINTy0_chW9w3QtiGTvk_ACLcB/s1600/0cdc.jpg"
   },
   {
-    _id: Race07,
+    _id: Race07, 
     name: "Race07",
     description: "Corriendo por Casa de Campo.",
-    length: 6000,
+    length: 13000,
     area: "CasaDeCampo",
     difficulty: "Medium",
     startPoint: {
       type: "Point",
-      coordinates: [40.4271919, -3.7460083]
+      coordinates: [40.4204, -3.73485]
     },
+    route: [{  
+      type: "Point",
+      coordinates: [40.4204, -3.73485]
+    },
+    {
+      type: "Point",
+      coordinates: [40.41622, -3.74601]
+    }, 
+    {
+      type: "Point",
+      coordinates: [40.40982, -3.75631]
+    },
+    {
+      type: "Point",
+      coordinates: [40.40459, -3.76369]
+    },
+    {
+      type: "Point",
+      coordinates: [40.41047, -3.77673]
+    }, 
+    {
+      type: "Point",
+      coordinates: [40.42302, -3.77725]
+    },
+    {
+      type: "Point",
+      coordinates: [40.4387, -3.78137]
+    }, 
+    {
+      type: "Point",
+      coordinates: [40.44353, -3.76866]
+    },
+    {
+      type: "Point",
+      coordinates: [40.44431, -3.75442]
+    },
+    {
+      type: "Point",
+      coordinates: [40.43669, -3.74438]
+    }, 
+    {
+      type: "Point",
+      coordinates: [40.43055, -3.7382]
+    },
+    {
+      type: "Point",
+      coordinates: [40.42349, -3.73287]
+    }
+  ],
     imgUrl: "https://madridsecreto.co/wp-content/uploads/2019/01/LagoCasaCampo_1.jpg"
   },
   {
     _id: Race08,
     name: "Race08",
     description: "Medio Maratón en Lago, Casa de Campo.",
-    length: 21000,
+    length: 8000,
     area: "CasaDeCampo",
     difficulty: "Hard",
     startPoint: {
       type: "Point",
-      coordinates: [40.4219303, -3.7557433]
+      coordinates: [40.45962, -3.75712]
     },
+    route: [{  
+      type: "Point",
+      coordinates: [40.45962, -3.75712]
+    },
+    {
+      type: "Point",
+      coordinates: [40.44825, -3.74802]
+    }, 
+    {
+      type: "Point",
+      coordinates: [40.43885, -3.74304]
+    },
+    {
+      type: "Point",
+      coordinates: [40.42754, -3.73575]
+    },
+    {
+      type: "Point",
+      coordinates: [40.4199, -3.72279]
+    }, 
+    {
+      type: "Point",
+      coordinates: [40.40703, -3.72296]
+    },
+    {
+      type: "Point",
+      coordinates: [40.39801, -3.71292]
+    }, 
+    { 
+      type: "Point",
+      coordinates: [40.39238, -3.70219]
+    }
+  ],
     imgUrl: "https://lh5.googleusercontent.com/p/AF1QipOAXbPxUCGOdE5Z6BTrzFSzuzztNp0RbEy98XBM=w408-h272-k-no"
   },
   {
     _id: Race09,
     name: "Race09",
     description: "La cuesta un poco dura, destroza piernas.",
-    length: 13000,
+    length: 5000,
     area: "CasaDeCampo",
     difficulty: "Hard",
     startPoint: {
       type: "Point",
-      coordinates: [40.435012, -3.766529]
+      coordinates: [40.38163, -3.68876]
     },
+    route: [{  
+      type: "Point",
+      coordinates: [40.38163, -3.68876]
+    },
+    {
+      type: "Point",
+      coordinates: [40.38836, -3.69828]
+    }, 
+    {
+      type: "Point",
+      coordinates: [40.39516, -3.70695]
+    },
+    {
+      type: "Point",
+      coordinates: [40.39948, -3.71622]
+    },
+    {
+      type: "Point",
+      coordinates: [40.40791, -3.72274]
+    }
+  ],
     imgUrl: "https://lh5.googleusercontent.com/p/AF1QipMt5V0VP3aa9DlR5a6FMLd1ObeD6Z-7Ou4oyYOn=w408-h306-k-no"
   },
   {
@@ -149,21 +252,87 @@ let races = [{
     difficulty: "Medium",
     startPoint: {
       type: "Point",
-      coordinates: [40.4349395, -3.7805837]
+      coordinates: [40.44345, -3.75527]
     },
+    route: [{  
+      type: "Point",
+      coordinates: [40.44345, -3.75527]
+    },
+    {
+      type: "Point",
+      coordinates: [40.43666, -3.74841]
+    }, 
+    {
+      type: "Point",
+      coordinates: [40.42914, -3.74506]
+    },
+    {
+      type: "Point",
+      coordinates: [40.42176, -3.74635]
+    },
+    {
+      type: "Point",
+      coordinates: [40.4166, -3.75656]
+    },
+    {
+      type: "Point",
+      coordinates: [40.42581, -3.7642]
+    },
+    {
+      type: "Point",
+      coordinates: [40.4364, -3.76532]
+    },
+    {
+      type: "Point",
+      coordinates: [40.44378, -3.75656]
+    }
+  ],
     imgUrl: "https://lh5.googleusercontent.com/p/AF1QipOY2mHobtAbW1JjL22v5nImqcdIt7ZcC9WdBwaR=w428-h240-k-no"
   },
   {
     _id: Race11,
     name: "Race11",
     description: "Han cambiado el asfalto, muy recomendable a última hora del día.",
-    length: 5100,
+    length: 8100,
     area: "MadridRio",
     difficulty: "Easy",
     startPoint: {
       type: "Point",
-      coordinates: [40.4066513, -3.7227381]
+      coordinates: [40.4526, -3.77939]
     },
+    route: [{  
+      type: "Point",
+      coordinates: [40.4526, -3.77939]
+    },
+    {
+      type: "Point",
+      coordinates: [40.44391, -3.77999]
+    }, 
+    {
+      type: "Point",
+      coordinates: [40.43731, -3.78377]
+    },
+    {
+      type: "Point",
+      coordinates: [40.42888, -3.78738]
+    },
+    {
+      type: "Point",
+      coordinates: [40.42235, -3.78901]
+    },
+    {
+      type: "Point",
+      coordinates: [40.41621, -3.79055]
+    },
+    {
+      type: "Point",
+      coordinates: [40.41072, -3.78686]
+    },
+    {
+      type: "Point",
+      coordinates: [40.40359, -3.78205]
+    }
+  ],
     imgUrl: "https://lh5.googleusercontent.com/p/AF1QipNV1mmGo742Swk5wqmK3J5R0XYYGJswnCoyF6l_=w408-h306-k-no"
   },
   {
@@ -175,8 +344,25 @@ let races = [{
     difficulty: "Easy",
     startPoint: {
       type: "Point",
-      coordinates: [40.3985017, -3.713465]
+      coordinates: [40.40399, -3.72317]
     },
+    route: [{  
+      type: "Point",
+      coordinates: [40.40399, -3.72317]
+    },
+    {
+      type: "Point",
+      coordinates: [40.39751, -3.71648]
+    }, 
+    {
+      type: "Point",
+      coordinates: [40.39313, -3.7248]
+    },
+    {
+      type: "Point",
+      coordinates: [40.3883, -3.73141]
+    }
+  ],
     imgUrl: "https://lh5.googleusercontent.com/p/AF1QipOCnjK3bnPf_u6XsRW-D16CC91z5zqmd9vcCqTX=w408-h306-k-no"
   },
   {
@@ -188,8 +374,25 @@ let races = [{
     difficulty: "Easy",
     startPoint: {
       type: "Point",
-      coordinates: [40.3884304, -3.6977381]
+      coordinates: [40.38941, -3.69923]
     },
+    route: [{  
+      type: "Point",
+      coordinates: [40.38941, -3.69923]
+    },
+    {
+      type: "Point",
+      coordinates: [40.39497, -3.70515]
+    }, 
+    {
+      type: "Point",
+      coordinates: [40.39, -3.71219]
+    },
+    {
+      type: "Point",
+      coordinates: [40.37999, -3.72635]
+    }
+  ],
     imgUrl: "https://lh5.googleusercontent.com/p/AF1QipOjlEysQnZKn_P2kXtvUyeAERCrHd0y17ikDF_i=w426-h240-k-no"
   }
 ];
